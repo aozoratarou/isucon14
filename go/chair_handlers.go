0f3e990ac7a69f687abf9605d0835250fa50aec4
muuran <muramuramuuran@gmail.com>
2024-12-08 08:02:50 +0000

    デバッグログ

--- go/chair_handlers.go
+++ go/chair_handlers.go
@@ -296,7 +296,7 @@ func chairPostRideStatus(w http.ResponseWriter, r *http.Request) {
 	defer tx.Rollback()
 
 	ride := &Ride{}
-	if err := tx.GetContext(ctx, ride, "SELECT * FROM rides WHERE id = ? FOR UPDATE", rideID); err != nil {
+	if err := tx.GetContext(ctx, ride, "SELECT * FROM rides WHERE id = ?", rideID); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			writeError(w, http.StatusNotFound, errors.New("ride not found"))
 			return
@@ -306,6 +306,7 @@ func chairPostRideStatus(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if ride.ChairID.String != chair.ID {
+		log.Printf("chairPostCoordinate: request chair id %s: actual chair id %s", ride.ChairID.String, chair.ID)
 		writeError(w, http.StatusBadRequest, errors.New("not assigned to this ride"))
 		return
 	}
